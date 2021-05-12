5ed905ef64b170f29ace072c51bb103b07ecdbd8
Leonlav <leonlav77@gmail.com>
2021-05-12 17:19:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import *
-
+from flask_socketio import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def home():
   return render_template("index.html")
+
+@app.route('/chat')
+def chat():
+  return render_template("chat.html")
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+    
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+    
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
