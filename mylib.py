5b5e94ba354089664ba749bf0faf12918c0dfcc7
Leonlav <leonlav77@gmail.com>
2021-05-13 09:50:37 +0200

    Update mylib.py

--- mylib.py
+++ mylib.py
@@ -9,6 +9,7 @@ def procitaj(gdje):     # funkcija samo za citanje
 def dodajNaKraj(sto,koji):
     with open(sto, "r+") as file:
         data = json.load(file)
-        data.update({sto:"twlve"})
+        k = koji.split('///')
+        data.update({k[1]:k[0]})
         file.seek(0)
         json.dump(data, file)
